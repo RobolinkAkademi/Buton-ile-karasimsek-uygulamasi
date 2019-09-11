1083f16dac78f70b65a94ff4653a4fb53a7e00cb
Robolink Teknoloji <55195037+RobolinkTeknoloji@users.noreply.github.com>
2019-09-11 17:07:40 +0300

    Add files via upload

---
+++ buton ile karasimsek uygulaması.ino
@@ -0,0 +1,33 @@
+
+#define buton 7
+int buton_durum = 0;
+void setup() {
+
+  for (int i = 2; i < 7; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+  pinMode(buton, INPUT);
+}
+void loop() {
+  buton_durum = digitalRead(buton);
+  if (buton_durum == 1)  {
+    for (int a = 2; a < 7; a++)
+    { digitalWrite(a, HIGH);
+      delay(100);
+      digitalWrite(a, LOW);
+    }
+    for (int b = 7; b > 2; b--)
+    { digitalWrite(b, HIGH);
+      delay(100);
+      digitalWrite(b, LOW);
+    }
+  }
+  else  {
+    for (int k = 2; k < 7; k++)
+    { digitalWrite(k, LOW);
+
+    }
+  }
+}
+
